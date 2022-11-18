5fb867474302673d9ebd982b5c254ac09a71ec90
dhp82 <42089795+dhp82@users.noreply.github.com>
2022-11-18 10:32:59 +0700

    Change Port

--- scripts/install.sh
+++ scripts/install.sh
@@ -115,9 +115,12 @@ IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
-
-FIRST_PORT=10000
-LAST_PORT=11000
+#echo "How many proxy do you want to create? Example 1000"
+#read COUNT
+#LAST_PORT=$(($FIRST_PORT + $COUNT))
+FIRST_PORT=28282
+#LAST_PORT=11000
+LAST_PORT=$(($FIRST_PORT + 1000))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
