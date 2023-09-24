0ed2cbf66df35b3a86a3463646a442dc59bf70c5
dhp82 <42089795+dhp82@users.noreply.github.com>
2023-09-24 23:13:31 +0700

    Update ipv6-with-port-none-password.sh

--- scripts/cloudfly/ipv6-with-port-none-password.sh
+++ scripts/cloudfly/ipv6-with-port-none-password.sh
@@ -94,18 +94,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-while :; do
-  read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
-  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
-    echo "OK! Valid number"
-    break
-  else
-    echo "Number out of range, try again"
-  fi
-done
-LAST_PORT=$(($FIRST_PORT + 750))
-echo "LAST_PORT is $LAST_PORT. Continue..."
+FIRST_PORT = 28282
+LAST_PORT=$(($FIRST_PORT + 1999))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
