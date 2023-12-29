f03375c96cf26391a8a5dfc6e0323ebd3950cd27
dhp82 <42089795+dhp82@users.noreply.github.com>
2023-12-29 09:19:27 +0700

    Update ipv6-with-port-password.sh

--- scripts/cloudfly/ipv6-with-port-password.sh
+++ scripts/cloudfly/ipv6-with-port-password.sh
@@ -100,7 +100,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 #LAST_PORT=$(($FIRST_PORT + $COUNT))
 FIRST_PORT=28282
 #LAST_PORT=11000
-LAST_PORT=$(($FIRST_PORT + 1999))
+LAST_PORT=$(($FIRST_PORT + 999))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
